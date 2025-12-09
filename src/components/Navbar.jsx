37f3f492ea8f7eb2ca24b760a8edebf26cb92996
haseebjaved4212 <m.haseebjavedhj@gmail.com>
2025-12-09 13:08:29 +0500

    Complete A section

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,13 +1,24 @@
-import React from 'react'
+import React from 'react'   
+import { navLinks } from '#constants/index.js'
 
 const Navbar = () => {
-  return (
-    <nav> 
-        <div>
-            <img src="/images/logo.svg" alt="logo" />
-        </div>
-    </nav>
-  )
+
+
+    return (
+        <nav>
+            <div>
+                <img src="/images/logo.svg" alt="logo" />
+                <p className='font-bold '> Haseeb's Portfolio </p>
+                <ul>
+                    {navLinks.map(({id, name}) => (
+                        <li key={id}>
+                           <p>{name}</p>
+                        </li>
+                    ))}
+                </ul>
+            </div>
+        </nav>
+    )
 }
 
 export default Navbar
---
+++ src/constants/index.js
@@ -0,0 +1,18 @@
+
+const navLinks = [
+    {
+        id: 1,
+        name: "Portfolio",
+
+    },{
+        id: 2,
+        name: "Contact ",
+
+    },{
+        id: 3,
+        name: "Projects",
+
+    }
+]
+
+export  {navLinks}
