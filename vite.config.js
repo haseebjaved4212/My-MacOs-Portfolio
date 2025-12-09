bd8fd2a834ab1f14021bb9f768ff0efdc9760899
haseebjaved4212 <m.haseebjavedhj@gmail.com>
2025-12-09 12:46:53 +0500

    Complete A section

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite' 
+import tailwindcss from '@tailwindcss/vite'
+import {resolve, dirname} from 'path' 
+import {fileURLToPath} from 'url'
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      '#components': resolve(dirname(fileURLToPath(import.meta.url)), 'components'),
+      '#constants': resolve(dirname(fileURLToPath(import.meta.url)), 'constants'),
+      '#hoc': resolve(dirname(fileURLToPath(import.meta.url)), 'hoc'),
+      '#windows': resolve(dirname(fileURLToPath(import.meta.url)), 'windows'),
+    },
+  },
 })
