ec76ce43a80531e37e9e84e5578bc76f4efb110e
haseebjaved4212 <m.haseebjavedhj@gmail.com>
2025-12-12 12:01:02 +0500

    Complete A section

--- src/components/Dock.jsx
+++ src/components/Dock.jsx
@@ -1,10 +1,26 @@
 import React, { useRef } from 'react'
+import { dockApps } from '#constants/index.js'
+
+
 const Dock = () => {
     const dockRef = useRef(null);
-  return (
-    <section id='dock'>
-        <div ref={dockRef} className="dock-container"></div>
-    </section>
-  )
+    return (
+        <section id='dock'>
+            <div ref={dockRef} className="dock-container">
+                {dockApps.map(({ id, name, icon, canOper }) =>
+                (
+                    <div key={id} className='relative flex justify-center '>
+                        <button type='button' className=' dock-icon' aria-label={name}
+                        data-tooltip-id="dock-tooltip"
+                        data-tooltip-content={name}
+                        data-tooltip-delay-show={150}
+                        disabled={!canOper}
+                        onClick={() => window.open()}
+                        ></button>
+                    </div>
+                ))}
+            </div>
+        </section>
+    )
 }
 export default Dock
