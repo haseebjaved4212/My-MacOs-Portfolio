a337b17f805b1431eaf0132fc9570ce6995fea31
haseebjaved4212 <m.haseebjavedhj@gmail.com>
2025-12-14 11:48:09 +0500

    feat: Add Zustand store for managing window states and Z-index.

--- src/store/windows.js
+++ src/store/windows.js
@@ -1,5 +1,9 @@
+import { INITIAL_Z_INDEX, WINDOW_CONFIG } from '#constants'
 import { create } from 'zustand'
+import {immer} from 'zustand/middleware/immer'
 
-const useWindowStore = create ((set ) => {
-
-})
+const useWindowStore = create(immer((set) => ({
+    windows: WINDOW_CONFIG,
+    nextZIndex: INITIAL_Z_INDEX + 1,
+}
+)))
