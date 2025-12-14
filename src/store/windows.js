f516ac8c5bf64cac98d788604902b0dbf714a152
haseebjaved4212 <m.haseebjavedhj@gmail.com>
2025-12-14 12:40:21 +0500

    Complete A section

--- src/store/windows.js
+++ src/store/windows.js
@@ -1,9 +1,31 @@
 import { INITIAL_Z_INDEX, WINDOW_CONFIG } from '#constants'
 import { create } from 'zustand'
-import {immer} from 'zustand/middleware/immer'
+import { immer } from 'zustand/middleware/immer';
 
 const useWindowStore = create(immer((set) => ({
     windows: WINDOW_CONFIG,
     nextZIndex: INITIAL_Z_INDEX + 1,
-}
-)))
+
+    openWindow: (windowKey, data = null) => set((state) => {
+        const win = state.window[windowKey];
+        win.isOpen = true;
+        win.zIndex = state.nextZIndex;
+        win.data = data ?? win.data;
+        state.nextZIndex++;
+    }),
+    closeWindow: (windowKey, data = null) => set((state) => {
+        const win = state.window[windowKey];
+        win.isOpen = false;
+        win.zIndex = INITIAL_Z_INDEX;
+        win.data = null;
+
+    }),
+    focusWindow: (windowKey, data = null) => set((state) => {
+        const win = state.window[windowKey];
+        win.zIndex = state.nextZIndex;
+        state.nextZIndex++;
+    }),
+})),
+);
+
+export default useWindowStore;
