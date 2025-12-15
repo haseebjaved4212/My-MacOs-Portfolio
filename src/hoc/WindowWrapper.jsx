3fce7afa1d224c91596357f7fb03ddb020b59080
haseebjaved4212 <m.haseebjavedhj@gmail.com>
2025-12-15 10:17:55 +0500

    Complete A section

--- src/hoc/WindowWrapper.jsx
+++ src/hoc/WindowWrapper.jsx
@@ -4,17 +4,21 @@ import { useRef } from "react"
 
 const WindowWrapper = (Component, windowKey) => {
 
-    const wrapped = (props) => {
+    const Wrapped = (props) => {
         const { windows, focusWindow } = useWindowStore();
         const { isOpen, zIndex } = windows[windowKey];
 
         const ref = useRef(null);
 
-        return <section id={windowKey} ref={ref} style={{ zIndex, }}></section>
-    }
-    return (
-        <div></div>
-    )
+        return (<section id={windowKey} ref={ref} style={{ zIndex, }} className="absolute ">
+            <Component {...props} />
+        </section>
+        )
+    };
+
+    Wrapped.displayName = `WindowWrapper(${Component.displayName || Component.name || 'Component'})`;
+    return Wrapped;
+
 }
 
 export default WindowWrapper
