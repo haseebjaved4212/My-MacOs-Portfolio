5ac5d1b0c0987164baa69552c31e8819b5a000db
haseebjaved4212 <m.haseebjavedhj@gmail.com>
2025-12-17 09:55:23 +0500

    feat: add Finder window component

--- src/windows/Finder.jsx
+++ src/windows/Finder.jsx
@@ -1,8 +1,18 @@
+import { WindowControls } from "#components"
+import WindowWrapper from "#hoc/WindowWrapper"
+import { Search } from "lucide-react"
 
 const Finder = () => {
   return (
-    <div>Finder</div>
+    <>
+    
+    <div id="window-header" className="flex items-center justify-between px-4 py-3 rounded-t-lg bg-gray-50 border-b border-gray-200 select-none text-sm text-gray-400">
+        <WindowControls target="finder"/>
+        <Search  className=" p-1 hover:bg-gray-200 rounded hover:cursor-default"/>
+
+    </div></>
   )
 }
 
-export default Finder
+const FinderWindow = WindowWrapper(Finder , "finder");
+export default FinderWindow
