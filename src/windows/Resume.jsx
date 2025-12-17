38ae297d374235aa62a6921db143a79e25b9f4b9
haseebjaved4212 <m.haseebjavedhj@gmail.com>
2025-12-17 09:30:40 +0500

    feat: add resume viewer window component and PDF file

--- src/windows/Resume.jsx
+++ src/windows/Resume.jsx
@@ -4,6 +4,7 @@ import { Download } from 'lucide-react';
 import { Document, Page, pdfjs } from 'react-pdf';
 
 import 'react-pdf/dist/Page/AnnotationLayer.css';
+import 'react-pdf/dist/Page/TextLayer.css';
 
 pdfjs.GlobalWorkerOptions.workerSrc = new URL(
     'pdfjs-dist/build/pdf.worker.min.mjs',
@@ -13,17 +14,17 @@ pdfjs.GlobalWorkerOptions.workerSrc = new URL(
 const Resume = () => {
     return (
         <>
-            <div id='wondow-header' className='flex items-center justify-between px-4 py-3 rounded-t-lg bg-gray-50 border-b border-gray-200 select-none text-sm text-gray-400'>
+            <div id='window-header' className='flex items-center justify-between px-4 py-3 rounded-t-lg bg-gray-50 border-b border-gray-200 select-none text-sm text-gray-400'>
 
                 <WindowControls target="resume" />
                 <h2>Resume.pdf</h2>
-                <a href="files/My_Resume.pdf" title='Download Resume' download="MY_Resume.pdf" className='cursor-pointer'>
+                <a href="files/My-Cv.pdf" title='Download Resume' download="My-Cv.pdf" className='cursor-pointer'>
                     <Download className="p-1 hover:bg-gray-200 rounded hover:cursor-default" /></a>
 
 
             </div>
-            <Document file="files/MY_Resume.pdf" >
-                <Page pageNumber={1} renderTextLayer renderAnnotationLayer />
+            <Document file="files/My-Cv.pdf" >
+                <Page pageNumber={1 } renderTextLayer renderAnnotationLayer />
             </Document>
 
         </>
