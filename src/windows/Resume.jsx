12de38193a1fd339801ef5a42ef4ff551e26d3cc
haseebjaved4212 <m.haseebjavedhj@gmail.com>
2025-12-17 09:04:13 +0500

    feat: Add new Resume window component with download functionality.

--- src/windows/Resume.jsx
+++ src/windows/Resume.jsx
@@ -1,18 +1,20 @@
 import { WindowControls } from '#components/index.js'
 import WindowWrapper from '#hoc/WindowWrapper'
+import { Download } from 'lucide-react';
 const Resume = () => {
-  return (
-   <>
-   <div id='wondow-header' className='flex items-center justify-between px-4 py-3 rounded-t-lg bg-gray-50 border-b border-gray-200 select-none text-sm text-gray-400'>
+    return (
+        <>
+            <div id='wondow-header' className='flex items-center justify-between px-4 py-3 rounded-t-lg bg-gray-50 border-b border-gray-200 select-none text-sm text-gray-400'>
 
-    <WindowControls  target="resume"/>
-    <h2>Resume.pdf</h2>
-    <a href="files/My_Resume.pdf" download="My_Resume.pdf">Download</a>
-      
-    
-   </div>
-   </>
-  )
+                <WindowControls target="resume" />
+                <h2>Resume.pdf</h2>
+                <a href="files/My_Resume.pdf" title='Download Resume' download="My_Resume.pdf" className='cursor-pointer'>
+                    <Download className="p-1 hover:bg-gray-200 rounded hover:cursor-default" /></a>
+
+
+            </div>
+        </>
+    )
 }
 
 const ResumeWindow = WindowWrapper(Resume, "resume");
