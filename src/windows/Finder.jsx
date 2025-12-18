7493547b7b55b95a44a6dce05c1b5e474f24ba03
haseebjaved4212 <m.haseebjavedhj@gmail.com>
2025-12-18 09:25:07 +0500

    feat: implement initial Finder window component with sidebar navigation and window controls.

--- src/windows/Finder.jsx
+++ src/windows/Finder.jsx
@@ -7,6 +7,22 @@ import clsx from "clsx";
 
 const Finder = () => {
   const { activeLocations: activeLocation, setActiveLocation } = useLocationStore();
+
+  const renderList = (name, items) =>
+    <div className="flex flex-col">
+      <h3>{name}</h3>
+
+      <ul>
+        {items.map((item) => (
+          <li key={item.id} onClick={() => setActiveLocation(item)}
+            className={clsx(item.id === activeLocation.id ? "active" : "not-active")}>
+            <img src={item.icon} alt={item.name} className="w-4" />
+            <p className="text-sm font-medium truncate">{item.name}</p>
+          </li>
+        ))}
+      </ul>
+    </div>
+
   return (
     <>
 
@@ -18,35 +34,22 @@ const Finder = () => {
       <div className="bg-white flex h-full">
         <div className="sidebar w-3/12 flex-none bg-gray-50 border-r border-gray-200 flex flex-col p-5">
 
-          <div className="">
-            <h3>Favorites</h3>
-            <ul>
-              {Object.values(locations).map((item) => (
-                <li key={item.id} onClick={() => setActiveLocation(item)}
-                  className={clsx(item.id === activeLocation.id ? "active" : "not-active")}>
-                  <img src={item.icon} alt={item.name} className="w-4" />
-                  <p className="text-sm font-medium truncate">{item.name}</p>
-                </li>
-              ))}
-            </ul>
 
-          </div> <div className="">
-            <h3>Work</h3>
-            <ul>
-              {locations.work.children.map((item) => (
-                <li key={item.id} onClick={() => setActiveLocation(item)}
-                  className={clsx(item.id === activeLocation.id ? "active" : "not-active")}>
-                  <img src={item.icon} alt={item.name} className="w-4" />
-                  <p className="text-sm font-medium truncate">{item.name}</p>
-                </li>
-              ))}
-            </ul>
+          {renderList("Favorites", Object.values(locations))}
 
-          </div>
+
+
+          {renderList("Work", locations.work.children)}
+
 
         </div>
 
       </div>
+      <ul className="col-span-10 space-y-3">
+
+      </ul>
+
+
     </>
   )
 }
