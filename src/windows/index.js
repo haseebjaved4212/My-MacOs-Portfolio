a9b3d8de7d1f2a2a2a471cb68b686277b3836f60
haseebjaved4212 <m.haseebjavedhj@gmail.com>
2025-12-19 09:04:53 +0500

    feat: Add Contact component and export it from the windows module.

--- src/windows/index.js
+++ src/windows/index.js
@@ -3,5 +3,6 @@ import Safari from "./Safari";
 import Resume from "./Resume";
 import Finder from "./Finder";
 import Text from "./Text";
-import Image from "./Image";
-export { Terminal, Safari, Resume, Finder, Text, Image };
+import Image from "./Image";    
+import Contact from "./Contact";
+export { Terminal, Safari, Resume, Finder, Text, Image, Contact };
