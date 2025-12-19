680b148d1100e0f0a86ec004417aac406d1e8752
haseebjaved4212 <m.haseebjavedhj@gmail.com>
2025-12-19 12:04:39 +0500

    feat: Add Photos component, gallery data, and structured project/about me content.

--- src/constants/index.js
+++ src/constants/index.js
@@ -200,19 +200,19 @@ const photosLinks = [
 const gallery = [
   {
     id: 1,
-    img: "/images/gal1.png",
+    img: "/images/Haseeb-1.jpeg",
   },
   {
     id: 2,
-    img: "/images/gal2.png",
+    img: "/images/Haseeb-2.jpeg",
   },
   {
     id: 3,
-    img: "/images/gal3.png",
+    img: "/images/Haseeb-3.jpeg",
   },
   {
     id: 4,
-    img: "/images/gal4.png",
+    img: "/images/Logo.jpeg",
   },
 ];
 
